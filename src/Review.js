bf591f465c5ed0a83650fa896e59a265d842842f
Mostafa <mosi3883@gmail.com>
2022-05-01 22:33:07 +0430

    prev and next review :)

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,48 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const fixIndex = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const prevPerson = () => {
+    setIndex((prevIndex) => fixIndex(prevIndex - 1));
+  };
+
+  const nextPerson = () => {
+    setIndex((prevIndex) => fixIndex(prevIndex + 1));
+  };
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>suprise me</button>
+    </article>
+  );
 };
 
 export default Review;
